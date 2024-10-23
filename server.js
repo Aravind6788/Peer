d8d427784cfc6dc3d2567813c8a677edc166c18b
Aravind <aravind6788@gmail.com>
2024-10-24 01:17:50 +0530

    peer

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const { PeerServer } = require("peer");
 const peerServer = PeerServer({
   port: 443,
   path: "/peerjs",
-  cors: {
-    origin: "https://group-meet-e8y3.onrender.com", 
-    methods: ["GET", "POST"],
-  },
+  allow_discovery: true,
 });
 
 console.log("Peer server running on port 3001");
